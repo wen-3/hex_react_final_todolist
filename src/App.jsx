193b70a5ea75f44f175a9d3950a5e1375a451d1e
wen-3 <sd63324@gmail.com>
2024-09-23 20:00:02 +0800

    Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
+import { NavLink, Route, Routes } from "react-router-dom"
+import SignUp from "./SignUp"
+import Login from "./Login"
+import Todo from "./Todo"
 
 function App() {
+  const navStyle = ({ isActive }) => {
+    return {
+      color: isActive && 'red'
+    }
+  }
 
   return (
     <>
-      React
+      <nav>
+        <NavLink to="/" style={navStyle}>註冊</NavLink>
+        <NavLink to="/login" style={navStyle}>登入</NavLink>
+        <NavLink to="/todo" style={navStyle}>Todo</NavLink>
+      </nav>
+      <Routes>
+        {/* 路由表 */}
+        <Route path="/" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/todo" element={<Todo />} />
+      </Routes>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './Todo.jsx'
+import { HashRouter } from 'react-router-dom'
+
+import App from './App.jsx'
 import './main.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </StrictMode>,
 )
