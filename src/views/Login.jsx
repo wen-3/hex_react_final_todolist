965396113c93db2041236cd57375df93471cf4ce
wen-3 <sd63324@gmail.com>
2024-09-25 18:27:43 +0800

    input 雙向綁定 & login  api 串接

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,23 +1,77 @@
+import { useState } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
 
 const { VITE_API_URL } = import.meta.env;
 
 const Login = () => {
+    const [formData, setFormData] = useState({
+        email: '',
+        password: ''
+    })
+
+    const handelOnChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value
+        })
+    }
+
     const navigate = useNavigate();
 
-    const clickLogin = (e) => {
-        navigate('/todo');
+    const clickLogin = async (e) => {
+        const url = `${VITE_API_URL}/users/sign_in`
+
+        try {
+            const res = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify(formData)
+            })
+
+            if (res.ok) {
+                // const data = await res.json();
+                // const token = data.token;
+                
+                const { token, nickname } = await res.json();
+                
+                // // 寫入 cookie
+                document.cookie = `loginToken=${token}`;
+                document.cookie = `loginNickname=${nickname}`;
+
+                alert('登入成功');
+                navigate('/todo');
+            } else {
+                switch (res.status) {
+                    case 400:
+                        alert('登入失敗：欄位不可為空');
+                        break;
+                    case 401:
+                        alert('登入失敗：帳號密碼驗證錯誤');
+                        break;
+                    case 404:
+                        alert('登入失敗：用戶不存在');
+                        break;
+                }
+            }
+        } catch (err) {
+            console.log(err);
+        }
     }
 
     return (<>
         <div>
             <form className="formControls" action="index.html">
                 <h2 className="formControls_txt">最實用的線上代辦事項服務</h2>
+
                 <label className="formControls_label" htmlFor="email">Email</label>
-                <input className="formControls_input" type="text" id="email" name="email" placeholder="請輸入 email" required />
-                <span>此欄位不可留空</span>
-                <label className="formControls_label" htmlFor="pwd">密碼</label>
-                <input className="formControls_input" type="password" name="pwd" id="pwd" placeholder="請輸入密碼" required />
+                <input className="formControls_input" type="text" id="email" name="email" placeholder="請輸入 email" onChange={handelOnChange} required />
+
+                <label className="formControls_label" htmlFor="password">密碼</label>
+                <input className="formControls_input" type="password" name="password" id="password" placeholder="請輸入密碼" onChange={handelOnChange} required />
+
                 <input className="formControls_btnSubmit" type="button" onClick={clickLogin} value="登入" />
                 <NavLink to='/signup' className='formControls_btnLink'>註冊帳號</NavLink>
             </form>
